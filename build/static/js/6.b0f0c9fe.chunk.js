7d1cb003c73a415e92586b724136eb0ca00d2991
GeorgeNikas <geknikas@gmail.com>
2022-10-03 20:32:54 +0300

    new deployment

---
+++ build/static/js/6.b0f0c9fe.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonprouter-1"]=this["webpackJsonprouter-1"]||[]).push([[6],{54:function(e,t,n){"use strict";n.r(t);var r=n(1);t.default=function(){return Object(r.jsx)("div",{className:"centered",children:Object(r.jsx)("p",{children:"Page not found!"})})}}}]);
+//# sourceMappingURL=6.b0f0c9fe.chunk.js.map
